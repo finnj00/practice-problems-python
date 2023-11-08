8aca13879be5a5978b539e3385998ac9fdef1984
Finnigan Johnson <finnj00@uw.edu>
2023-11-07 18:53:45 -0800

    Scratch work for Longest Increasing Subsequence problem

---
+++ lisTest.py
@@ -0,0 +1,12 @@
+import longestIncreasingSequence as lis
+
+def test123():
+    sol = lis.Solution()
+    nums = [1, 2, 3]
+    sol.lis(nums)
+
+def main():
+    test123()
+
+if __name__ == '__main__':
+    main()
---
+++ longestIncreasingSequence.py
@@ -0,0 +1,4 @@
+class Solution:
+    def lis(self, nums) -> int:
+        print('In method')
+        return
